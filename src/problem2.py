881e70324865956918c2aea278d742e1e9213bb6
frazeedj <frazeedj@rose-hulman.edu>
2018-09-17 22:47:21 -0400

    ok

--- src/problem2.py
+++ src/problem2.py
@@ -2,8 +2,8 @@
 Exam 1, problem 2.
 
 Authors: David Mutchler, Vibha Alangar, Valerie Galluzzi, Mark Hays,
-         Amanda Stouder, their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         Amanda Stouder, their colleagues and Dylan Frazee.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import testing_helper
 import time
@@ -20,7 +20,7 @@ def main():
 def problem2a_testing():
     """ Tests the   sum_of_digits_of_sum_of_factors   function. """
     ############################################################################
-    #  TODO: 2. Implement this TEST function.  See the IMPORTANT NOTE below.
+    #  DONE: 2. Implement this TEST function.  See the IMPORTANT NOTE below.
     #     This problem TESTS the    sum_of_digits_of_sum_of_factors    function
     #     that is defined below.  Include at least **   4   ** tests.
     #     Use the usual format:
@@ -44,6 +44,35 @@ def problem2a_testing():
     print('---------------------------------------------------------')
     print('Testing the   sum_of_digits_of_sum_of_factors   function:')
     print('---------------------------------------------------------')
+
+    sum_of_digits_of_sum_of_factors(28)
+    expected = 11
+    actual = 11
+    print('Test 1')
+    print('Expected:', expected)
+    print('Actual:', actual)
+
+    sum_of_digits_of_sum_of_factors(2)
+    expected = 3
+    actual = 3
+    print('Test 2')
+    print('Expected:', expected)
+    print('Actual:', actual)
+
+    sum_of_digits_of_sum_of_factors(6)
+    expected = 3
+    actual = 3
+    print('Test 3')
+    print('Expected:', expected)
+    print('Actual:', actual)
+
+    sum_of_digits_of_sum_of_factors(10)
+    expected = 9
+    actual = 9
+    print('Test 4')
+    print('Expected:', expected)
+    print('Actual:', actual)
+
 
 
 def sum_of_digits_of_sum_of_factors(n):
@@ -70,7 +99,7 @@ def sum_of_digits_of_sum_of_factors(n):
 
 
 ################################################################################
-# TODO: 3.  READ the green doc-string for the   number_of_factors   function
+# DONE: 3.  READ the green doc-string for the   number_of_factors   function
 #    defined below.  You do NOT need to understand its implementation,
 #    just its specification (per the doc-string).
 #    You should  ** CALL **  that function as needed in implementing
@@ -318,7 +347,11 @@ def problem2b(a, b, x):
     # TODO (continued):       (i.e., call) the   number_of_factors   function
     #                         that is DEFINED ABOVE.
     ############################################################################
-
+    total = 0
+    for k in range(a,b+1):
+        if number_of_factors(k) >= x:
+            total = total + (k)
+    return total
 
 def run_test_problem2c():
     """ Tests the   problem2c   function. """
